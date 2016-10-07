1c0c7b57a9af0013a56b363aa4eb67188f0289d7
Dong Joon Kim <dongjoonkim@Dong-Joons-MacBook-Pro.local>
2016-10-06 21:34:31 -0700

    Week 2

---
+++ Week2/MediatorExample.java
@@ -0,0 +1,99 @@
+/**
+ * @author		DJ Kim <djkim@cs.ucla.edu>
+ *
+ * CS 130 Fall 2016
+ *
+ * Created to illustrate a real-life example of Mediator Design Pattern
+ * Air Traffic Controller controls all of the incoming flights and make
+ * sure the runway is open for landing. All of the incoming flights 
+ * communicates to the ATC and ATC communicates to runway. Incoming flights
+ * and runway never directly communicates.
+ *
+ */
+
+// Mediator
+public interface Mediator {
+	public void addRunway(Runway runway);
+
+	public void addFlight(Flight flight);
+
+	public void removeFlight(Flight flight);
+
+	public boolean isLandingOk();
+
+	public void setLandingStatus(boolean isLandingOk);
+}
+
+// ConcreteMediator
+public class AirTrafficController implements Mediator {
+	private Arraylist<Flight> flights = new ArrayList();
+	private Runway runway;
+	public boolean isLandingOk;
+
+	public void addRunway(Runway runway) {
+		this.runway = runway;
+	}
+
+	public void addFlight(Flight flight) {
+		flights.add(flight);
+	}
+
+	public void removeFlight(Flight flight) {
+		flights.remove(flight);
+	}
+
+	public boolean isLandingOk() {
+		return isLandingOk;
+	}
+
+	public void setLandingOk(boolean isLandingOk) {
+		this.isLandingOk = isLandingOk;
+	}
+}
+
+// Colleague
+public interface Colleague {
+	void land();
+}
+
+// ConcreteColleague
+public class Flight implements Colleague {
+	private Mediator mediator;
+
+	public Flight(Mediator mediator) {
+		this.mediator = mediator;
+	}
+
+	public void land() {
+		// cleared to land
+		if (mediator.isLandingOk()) {
+			System.out.println("Cleared to land. Landing in progress");
+			// since the flight is about to land on the runway, set the landing status to false
+			mediator.setLandingStatus(false);
+		} else {
+			// not cleared to land
+			System.out.println("Not cleared to land. Waiting for the clear");
+		}
+	}
+
+	public void landingDone() {
+		System.out.println("Successfully landed");
+		mediator.setLandingStatus(true);
+		// remove the flight from the landing queue
+		mediator.removeFlight(this);
+	}
+}
+
+public class Runway implements Colleague {
+	private Mediator mediator;
+
+	public Runway(Mediator mediator) {
+		this.mediator = mediator;
+		mediator.setLandingStatus(true);
+	}
+
+	public void land() {
+		System.out.println("Runway open for landing");
+		mediator.setLandingStatus(true);
+	}
+}
---
+++ Week2/ObserverExample.java
@@ -0,0 +1,60 @@
+/**
+ * @author		DJ Kim <djkim@cs.ucla.edu>
+ *
+ * CS 130 Fall 2016
+ *
+ * Created to illustrate a real-life example of Observer Design Pattern
+ * Users who are following a group post get notification when the post 
+ * gets new like / comment.
+ *
+ */
+
+// subject
+public interface FacebookPost {
+	public void registerObserver(FacebookUser user);
+	public void removeObserver(FacebookUser user);
+	public void notifyObservers();
+}
+
+// observer
+public interface FacebookUser {
+	public void update();
+}
+
+
+// concreteSubject
+public class FacebookGroupPost implements FacebookPost {
+	private ArrayList<FacebookUser> followers = new ArrayList();
+
+	public void registerObserver(FacebookUser follower) {
+		followers.add(follower);
+	}
+
+	public void removeObserver(FacebookUser user) {
+		followers.remove(follower);
+	}
+
+	public void notifyObservers(String message) {
+		for (FacebookUser follower : followers) {
+			follower.update(message);
+		}
+	}
+
+	// notification sent when someone likes the post
+	public void newLike() {
+		notifyObservers("New Like");
+	}
+
+	// notification sent when someone comments on the post
+	public void newComment() {
+		notifyObservers("New comment");
+	}
+}
+
+// concreteObserver
+public class FacebookGroupMember implements FacebookUser {
+	public void update(String message) {
+		// You got a new notification! Notification bar has been updated
+		System.out.println("Not cleared to land. Waiting for the clear");
+	}
+}
